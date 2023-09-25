64af2ba6604ebdb685bbe80c9bc6c20063e1f7f9
frekerzzzz <110891748+frekerzzzz@users.noreply.github.com>
2023-09-25 12:52:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let obj1 = {
+    name:"person1",
+    age:5
+}
+
+let obj2 = {
+    age:5,
+    name:"person1"
+}
+
+if(obj1 == obj2){
+    console.log("they are same")
+}else{
+    console.log("they are not same")
+}
